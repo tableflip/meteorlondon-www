1ceffd28d3f4b95f95baf4c34b6a4eb538af44a4
olizilla <oli@zilla.org.uk>
2014-10-07 14:44:59 +0100

    Add fast-render

--- both/router.js
+++ both/router.js
@@ -1,5 +1,5 @@
 Router.map(function () {
   this.route('login', {path: '/login', template: 'login'})
-  this.route('home',  {path: '/', controller: 'HomeController'})
+  this.route('home',  {path: '/', controller: 'HomeController', fastRender: true})
   this.route('admin', {path: '/admin', controller: 'AdminController', loginRequired: {name: 'login', shouldRoute: false} })
 })
